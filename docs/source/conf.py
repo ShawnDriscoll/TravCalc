cc5c3fe41e3740c2c77eff2202d5b9cdbc9d3472
ShawnDriscoll <shawndriscoll@hotmail.com>
2021-04-07 19:12:52 -0700

    tutorial

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -45,7 +45,7 @@ author = u'SHONNER CORP'
 # The short X.Y version.
 version = '3.0'
 # The full version, including alpha/beta/rc tags.
-release = '3.0.6'
+release = '3.0.6b'
 
 # There are two options for replacing |today|: either, you set today to some
 # non-false value, then it is used:
